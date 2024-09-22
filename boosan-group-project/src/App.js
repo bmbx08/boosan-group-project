c7db68485f30cfa1699ca349ec199017d1757b77
bmbx08 <bmbx08@bu.ac.kr>
2024-09-22 21:09:44 +0900

    create project

--- boosan-group-project/src/App.js
+++ boosan-group-project/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import map from "./common/images/boosan-map.png"
 
 function App() {
   return (
     <div>
+      <div className='map-container'>
+      <img src={map} className='map'/>
+      </div>
       
     </div>
   );
