8bc588f66052c5f05491cba4f3f55510c4b309ab
bmbx08 <bmbx08@bu.ac.kr>
2024-09-26 01:54:42 +0900

    api call success

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
 import "./App.css";
 import map from "./common/images/boosan-map.png";
 
+let tokenKey = `PbshnZKCA1YUtih0GQHG4aIUbS4ZX6gBXVSW0WboMeHxBIPic7j7j6jA08TzW4jP`
+let imageURL = `/api/food/img`
+
 function App() {
+  const getFoodImage = async() => {
+    let url = `https://busan-food.openapi.redtable.global${imageURL}?serviceKey=${tokenKey}`
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log("data",data);
+  }
+
+  useEffect(()=>{
+    getFoodImage();
+  },[]);
+
   return (
     <div>
       <div className="map-container">
