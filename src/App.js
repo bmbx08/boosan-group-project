6901eca0250a6c27461eeef2fa851430e1aed546
bmbx08 <bmbx08@bu.ac.kr>
2024-10-02 22:26:13 +0900

    show rstr by location

--- src/App.js
+++ src/App.js
@@ -6,129 +6,87 @@ import "./App.css";
 // import map from "./common/images/boosan-map.png";
 import northwest from "./common/images/northwest.png"
 import tourist from "./common/images/tourist.png"
+import boogi from "./common/images/Boogi.png"
 import restaurant from "./common/images/restaurant.png"
 import RstrModal from "./components/RstrModal";
 
-let tokenKey = `PbshnZKCA1YUtih0GQHG4aIUbS4ZX6gBXVSW0WboMeHxBIPic7j7j6jA08TzW4jP`
-let rstrDataURL='/api/rstr'
-let rstrOperateURL='/api/rstr/oprt'
-let rstrQualityURL='/api/rstr/qlt'
-let rstrImageURL='/api/rstr/img'
-let foodImageURL = `/api/food/img`
-let menuURL= '/api/menu/korean'
-let menuDescURL= '/api/menu-dscrn/korean'
+
 
 
 function App() {
   const [show, setShow] = useState(false);
+  const [rstrData, setRstrData] = useState(null);
+  const [totalData, setTotalData]=useState(null);
+  const [keumData, setKeumData]= useState(null);
+  const [dongData, setDongData]= useState(null);
+  const [yeonData, setYeonData]= useState(null);
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  // const handleShow = (num) => setShow(true);
 
-  // var xhr = new XMLHttpRequest();
-  // var url = 'http://apis.data.go.kr/6260000/FoodService/getFoodKr'; /*URL*/
-  // var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'gbn2CWbfPq7J5Cn3UNX1uKBsgVCaCoH%2B7bsxs2v1db4LsayzwhOFUOWpjJypath3Qt3e0cfXciTFO1i6CX%2FVNQ%3D%3D'; /*Service Key*/
-  // queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
-  // queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /**/
-  // xhr.open('GET', url + queryParams);
-  // xhr.onreadystatechange = function () {
-  //     if (this.readyState == 4) {
-  //         alert('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
-  //     }
-  // };
-  // xhr.send('');
-
-  const getData = async()=>{
-    let key = 'gbn2CWbfPq7J5Cn3UNX1uKBsgVCaCoH%2B7bsxs2v1db4LsayzwhOFUOWpjJypath3Qt3e0cfXciTFO1i6CX%2FVNQ%3D%3D'
-    let url=`http://apis.data.go.kr/6260000/FoodService/getFoodKr?serviceKey=${key}&pageNo=1&numOfRows=1000&resultType=json`
-    let response = await fetch(url);
-    console.log(response);
-    let data = await response.json();
-    console.log(data);
+  const handleKeumShow = (num) =>{
+    setRstrData(keumData?keumData[num]:null); 
+    setShow(true);
+  }
+  const handleDongShow = (num) =>{
+    setRstrData(dongData?dongData[num]:null); 
+    setShow(true);
+  }
+  const handleYeonShow = (num) =>{
+    setRstrData(yeonData?yeonData[num]:null); 
+    setShow(true);
   }
 
+  //keum,dong,yeon
 
-  // // const [rstrData,setRstrData]= useState();
-  // let rstrData;
-  // let rstrID;
-  
+  const getData = async()=>{
+    try{
+      let key = 'gbn2CWbfPq7J5Cn3UNX1uKBsgVCaCoH%2B7bsxs2v1db4LsayzwhOFUOWpjJypath3Qt3e0cfXciTFO1i6CX%2FVNQ%3D%3D'
+      let url=`http://apis.data.go.kr/6260000/FoodService/getFoodKr?serviceKey=${key}&pageNo=1&numOfRows=1000&resultType=json`
+      let response = await fetch(url);
+      let data = await response.json();
+      console.log(data);
+      filterData(data);
+      setTotalData(data);
+      setRstrData(data.getFoodKr.item[0]);      
+    }catch(error){
+      console.log("error message",error);
+    }
+  }
 
-  // const getRstrData = async() => {
-  //   let url = `https://busan-food.openapi.redtable.global${rstrDataURL}?serviceKey=${tokenKey}`
-  //   let response = await fetch(url);
-  //   let data = await response.json();
-  //   // setRstrData(data.body[0]);
-  //   rstrData = data.body[0];  
-  //   rstrID = rstrData.RSTR_ID;
+  const filterData=(data)=>{
+    let keum = data.getFoodKr.item.filter((item)=>{
+      return item.GUGUN_NM === '금정구'
+    })
+    setKeumData(keum);
+    console.log("금정구 데이터", keum);
+    let dong = data.getFoodKr.item.filter((item)=>{
+      return item.GUGUN_NM === '동래구'
+    })
+    setDongData(dong);
+    console.log("동래구 데이터", dong);
+    let yeon = data.getFoodKr.item.filter((item)=>{
+      return item.GUGUN_NM === '연제구'
+    })
+    setYeonData(yeon);
+    console.log("연제구 데이터", yeon);
+  }
 
-  //   console.log("restaurant data",data);
-  //   console.log("first example",rstrData);
-  //   console.log("rstr ID",rstrID);
-  // }
-
-  // const getRstrOperateData = async() => {
-  //   let url = `https://busan-food.openapi.redtable.global${rstrOperateURL}?serviceKey=${tokenKey}`
-  //   let response = await fetch(url);
-  //   let data = await response.json();
-  //   console.log("rstr operate data",data);
-  // }
-
-  // const getRstrQualityData = async() => {
-  //   let url = `https://busan-food.openapi.redtable.global${rstrQualityURL}?serviceKey=${tokenKey}`
-  //   let response = await fetch(url);
-  //   let data = await response.json();
-  //   console.log("rstr quality data",data);
-  // }
-
-  // const getRstrImages = async() => {
-  //   let url = `https://busan-food.openapi.redtable.global${rstrImageURL}?serviceKey=${tokenKey}`
-  //   let response = await fetch(url);
-  //   let data = await response.json();
-  //   // let rstrImages = data.filter((img)=>{
-  //   //   return img.RSTR_ID == rstrID;
-  //   // })
-
-  //   console.log("rstr images",data);
-  //   // console.log("thisss rstr images", rstrImages);
-  // }
-
-  // const getMenuData = async() => {
-  //   let url = `https://busan-food.openapi.redtable.global${menuURL}?serviceKey=${tokenKey}`
-  //   let response = await fetch(url);
-  //   let data = await response.json();
-  //   console.log("menu data",data);
-  // }
-
-  // const getMenuDescData = async() => {
-  //   let url = `https://busan-food.openapi.redtable.global${menuDescURL}?serviceKey=${tokenKey}`
-  //   let response = await fetch(url);
-  //   let data = await response.json();
-  //   console.log("menu desc data",data);
-  // }
-
-  // const getFoodImages = async() => {
-  //   let url = `https://busan-food.openapi.redtable.global${foodImageURL}?serviceKey=${tokenKey}`
-  //   let response = await fetch(url);
-  //   let data = await response.json();
-  //   console.log("food images",data);
-  // }
-
-  
-  // const getAllData = ()=>{
-  //   getRstrData();
-  //   getRstrOperateData();
-  //   getRstrQualityData();
-  //   getRstrImages();
-  //   getMenuData();
-  //   getMenuDescData();
-  //   getFoodImages();
-  // }
-  
-  //1,5,9,10,11,12,13,14
+  const getAttractionData = async()=>{
+    try{
+      let key = 'gbn2CWbfPq7J5Cn3UNX1uKBsgVCaCoH%2B7bsxs2v1db4LsayzwhOFUOWpjJypath3Qt3e0cfXciTFO1i6CX%2FVNQ%3D%3D'
+      let url=`http://apis.data.go.kr/6260000/AttractionService/getAttractionKr?serviceKey=${key}&pageNo=1&numOfRows=1000&resultType=json`
+      let response = await fetch(url);
+      let data = await response.json();
+      console.log(data);    
+    }catch(error){
+      console.log("error message",error);
+    }
+  }
 
   useEffect(()=>{
-    // getAllData();
     getData();
+    getAttractionData();
   },[]);
 
   return (
@@ -136,15 +94,16 @@ function App() {
       <div className="map-container">
         {/* <img src={map} className="map" /> */}
         <img src={northwest} alt="busan-northwest-map" className="map" width="800"/>
-        <img src={tourist} alt="tourist-img" className="tourist" width="150"/>
-        <img src={restaurant} alt="restaurant-img1" className="food pos1" width="90" onClick={handleShow}/>
-        <img src={restaurant} alt="restaurant-img2" className="food pos2" width="90"/>
-        <img src={restaurant} alt="restaurant-img3" className="food pos3" width="90"/>
-        <img src={restaurant} alt="restaurant-img4" className="food pos4" width="90"/>
-        <img src={restaurant} alt="restaurant-img5" className="food pos5" width="90"/>
+        {/* <img src={tourist} alt="tourist-img" className="tourist" width="150"/> */}
+        <img src={boogi} alt="tourist-img" className="tourist" width="150"/> {/*부산시 소통캐릭터*/}
+        <img src={restaurant} alt="restaurant-img0" className="food pos1" width="90" onClick={()=>handleKeumShow(0)}/>
+        <img src={restaurant} alt="restaurant-img1" className="food pos2" width="90" onClick={()=>handleKeumShow(1)}/>
+        <img src={restaurant} alt="restaurant-img2" className="food pos3" width="90" onClick={()=>handleDongShow(0)}/>
+        <img src={restaurant} alt="restaurant-img3" className="food pos4" width="90" onClick={()=>handleDongShow(1)}/>
+        <img src={restaurant} alt="restaurant-img4" className="food pos5" width="90" onClick={()=>handleYeonShow(0)}/>
       </div>
-      
-      {/* <RstrModal data={rstrData} show={show} handleClose={handleClose}/>/ */}
+      {/* <div>{rstrData?.getFoodKr.item[0].TITLE}</div> */}
+      <RstrModal data={rstrData} show={show} handleClose={handleClose}/>
     </div>
   );
 }
--- src/components/RstrModal.jsx
+++ src/components/RstrModal.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import { Button, Modal } from "react-bootstrap";
 
 const RstrModal = ({data, show, handleClose}) => {
-  console.log("component data",data);
+  // console.log("component data",data);
   return (
     <Modal show={show} onHide={handleClose} centered>
       <Modal.Header closeButton>
-        <Modal.Title><h3>{data?.RSTR_NM}</h3><h5>{data?.BSNS_STATM_BZCND_NM}</h5></Modal.Title>
+        <Modal.Title><h3>{data?.RSTR_NM}</h3><h5>{data?.MAIN_TITLE}</h5></Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>주소: {data?.RSTR_RDNMADR}</h4>
-        <h4>번호: {data?.RSTR_TELNO}</h4><br/>
-        <h4><b>상세정보</b></h4>
-        <h4>{data?.RSTR_INTRCN_CONT}</h4>
+        <h4>주소: {data?.ADDR1}</h4>
+        <h4>번호: {data?.CNTCT_TEL}</h4>
+        <h4>운영시간: {data?.USAGE_DAY_WEEK_AND_TIME}</h4>
+        <h4>대표메뉴: {data?.RPRSNTV_MENU}</h4>
+        <img src={data?.MAIN_IMG_THUMB} /><br/><br/>
+        <h5><b>상세정보</b></h5>
+        <h5>{data?.ITEMCNTNTS}</h5>
+        
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
