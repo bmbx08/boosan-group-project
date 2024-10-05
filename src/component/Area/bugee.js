c1262f798138e22c2b50c53ed48353d5ad3b5f93
bmbx08 <152158238+bmbx08@users.noreply.github.com>
2024-10-06 05:59:53 +0900

    Revert "restored commit"

---
+++ src/component/Area/bugee.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import bugeeImage from '../../common/images/부산시마스코트부기.png'
+import './Bugee.css'
+const bugee = () => {
+  return (
+    <div className="bugee-container">
+          <div className="thought">
+          나는 오늘 너의 소통을 도와줄 부산 마스코트 부기야!
+          </div>
+          <img
+            src={bugeeImage}
+            alt="부산시 마스코트 부기"
+            className="bugee-image"
+          />
+        </div>
+  )
+}
+
+export default bugee
